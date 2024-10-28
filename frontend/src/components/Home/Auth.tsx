a6ae3b9cbe09d42eb2c52241ffc4c68cec0e9c4c
ryunosuke121 <ryunosuke.ito.121@gmail.com>
2024-10-28 13:56:04 +0900

    front: JIAのポートを変更

--- frontend/src/components/Home/Auth.tsx
+++ frontend/src/components/Home/Auth.tsx
@@ -3,7 +3,7 @@ import Card from '/@/components/UI/Card'
 
 const Auth = () => {
   const click = async () => {
-    location.href = `http://localhost:5000?callback=${location.protocol}//${location.host}`
+    location.href = `http://localhost:5001?callback=${location.protocol}//${location.host}`
   }
 
   return (
